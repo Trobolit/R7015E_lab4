6e15120339e6a012dd8b22430404d75247eb8a49
Robert Hedman <robert.hedman@mac.com>
2018-10-30 17:11:52 +0100

    init commit, data loaded

---
+++ loadData.m
@@ -0,0 +1,19 @@
+boom_angle_deg = importdata('boom_angle_deg.txt');
+boom_speed_deg_per_sec = importdata('boom_speed_deg_per_sec.txt');
+
+bucket_angle_deg = importdata('bucket_angle_deg.txt');
+bucket_speed_deg_per_sec = importdata('bucket_speed_deg_per_sec.txt');
+
+digging = importdata('digging.txt');
+
+drive_axle_speed_rps = importdata('drive_axle_speed_rps.txt');
+engine_speed_rps = importdata('engine_speed_rps.txt');
+
+force_lift_MN = importdata('force_lift_MN.txt');
+force_tilt_MN = importdata('force_tilt_MN.txt');
+
+gear_index = importdata('gear_index.txt');
+
+lift_joystick = importdata('lift_joystick.txt');
+throttle = importdata('throttle.txt');
+tilt_joystick = importdata('tilt_joystick.txt');
